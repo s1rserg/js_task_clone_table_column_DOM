225afc61e942a5f5e659fa1545041de5536e7d2a
sirserg <sirserg05@gmail.com>
2025-04-05 18:24:34 +0300

    fix: refactor task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,7 @@ const tableBody = table.tBodies[0];
 const tableFoot = table.tFoot;
 
 [...tableHead.rows, ...tableBody.rows, ...tableFoot.rows].forEach((row) => {
-  const rowCopy = row.children[1];
+  const clonedCell = row.children[1].cloneNode(true);
 
-  row.children[row.children.length - 1].insertAdjacentHTML(
-    'beforebegin',
-    rowCopy.outerHTML,
-  );
+  row.children[row.children.length - 1].before(clonedCell);
 });
