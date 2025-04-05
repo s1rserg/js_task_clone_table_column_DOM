e08395c37585ec42b217666d9ed65d868d088a7c
sirserg <sirserg05@gmail.com>
2025-04-05 18:18:41 +0300

    feat: add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const tableHead = table.tHead;
+const tableBody = table.tBodies[0];
+const tableFoot = table.tFoot;
+
+[...tableHead.rows, ...tableBody.rows, ...tableFoot.rows].forEach((row) => {
+  const rowCopy = row.children[1];
+
+  row.children[row.children.length - 1].insertAdjacentHTML(
+    'beforebegin',
+    rowCopy.outerHTML,
+  );
+});
